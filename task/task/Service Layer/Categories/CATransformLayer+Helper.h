fa9c2f359af203b1b5515a377ab7d8767e12a480
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-10-02 14:31:54 -0300

    A category to avoid 'changing property masksToBounds in transform-only layer, will have no effect' warning

---
+++ task/task/Service Layer/Categories/CATransformLayer+Helper.h
@@ -0,0 +1,13 @@
+//
+//  CATransformLayer+Helper.h
+//  task
+//
+//  Created by Jhonathan Wyterlin on 10/2/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface CATransformLayer (Helper)
+
+@end
---
+++ task/task/Service Layer/Categories/CATransformLayer+Helper.m
@@ -0,0 +1,37 @@
+//
+//  CATransformLayer+Helper.m
+//  task
+//
+//  Created by Jhonathan Wyterlin on 10/2/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "CATransformLayer+Helper.h"
+
+@implementation CATransformLayer (Helper)
+
+-(void)setOpaque:(BOOL)opaque {
+    return;
+}
+
+-(void)setEdgeAntialiasingMask:(unsigned int)edgeAntialiasingMask {
+    return;
+}
+
+-(void)setShadowColor:(CGColorRef)shadowColor {
+    return;
+}
+
+-(void)setMasksToBounds:(BOOL)masksToBounds {
+    return;
+}
+
+-(void)setFilters:(NSArray *)filters {
+    return;
+}
+
+-(void)setBackgroundFilters:(NSArray *)backgroundFilters {
+    return;
+}
+
+@end
